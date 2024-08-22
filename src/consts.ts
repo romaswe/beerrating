1837e34bc059674471db4c6953d9e4e272c192b7
Försth <sebastian.forsth@cgi.com>
2024-08-22 10:40:12 +0200

    New home page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Beer rating";
+export const SITE_TITLE = "Beer Rating";
 export const SITE_DESCRIPTION = "Welcome to beer side of the web!";
