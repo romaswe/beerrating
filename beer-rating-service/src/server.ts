98dda95e47df7ea68f05a7086fe2e314a22d7f2a
Sebastian <forsthsebbe@gmail.com>
2024-08-24 19:29:50 +0200

    added some headers and logging request times

--- beer-rating-service/src/server.ts
+++ beer-rating-service/src/server.ts
@@ -1,18 +1,37 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import morgan from 'morgan';
 import authRoutes from './routes/authRoutes';
 import beerRoutes from './routes/beerRoutes';
 import ratingRoutes from './routes/ratingRoutes';
 import sheetRoutes from './routes/sheetRoutes';
 import helmet from "helmet";
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Application = express();
 
+// Set up morgan for logging requests
+app.use(morgan('combined'));
+
+app.use(express.json());
+
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
+
+app.use((req: Request, res: Response, next: NextFunction): void => {
+    const start = process.hrtime();
+    res.on('finish', () => {
+        const [seconds, nanoseconds] = process.hrtime(start);
+        const durationInMilliseconds = (seconds * 1000 + nanoseconds / 1e6).toFixed(3);
+        console.log(`Request processed in ${durationInMilliseconds}ms`);
+    });
+    next();
+});
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/beers', beerRoutes);
