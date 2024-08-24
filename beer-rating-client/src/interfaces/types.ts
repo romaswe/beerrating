b2e27945240839487109351a4a5d98be793d178f
Sebastian <forsthsebbe@gmail.com>
2024-08-24 17:18:54 +0200

    Changed so we are fetching beers from local backend

--- beer-rating-client/src/interfaces/types.ts
+++ beer-rating-client/src/interfaces/types.ts
@@ -18,3 +18,22 @@ export interface StyleObj {
   topBeer?: BeerObj;
   avrageStyleRating?: number;
 }
+
+export interface BeerSheet {
+  headers: string[];
+  data: Array<string[]>;
+  styleArray: Array<Array<StyleArrayClass>>;
+}
+
+export interface StyleArrayClass {
+  style: string;
+  count: number;
+  topBeer: TopBeer;
+  avrageStyleRating: number;
+}
+
+export interface TopBeer {
+  name: string;
+  rating: number;
+  style: string;
+}
