18e29a7c4dac577e011b8ca42db59ea8414714ca
Sebastian <forsthsebbe@gmail.com>
2024-08-30 21:59:15 +0200

    fixed build problems

--- beer-rating-client-vue/src/components/BeerModal.vue
+++ beer-rating-client-vue/src/components/BeerModal.vue
@@ -1,57 +1,89 @@
 <template>
-    <div class="modal-overlay" @click.self="closeModal">
-        <div class="modal-content">
-            <button v-if="!isEditing && !isAddingRating && isLoggedIn" @click="toggleEditMode"
-                class="edit-button">Edit</button>
-            <template v-if="isEditing">
-                <!-- Use BeerForm for editing -->
-                <BeerForm :beer="beer" :isEdit="true" @submit="handleFormSubmit" @cancel="toggleEditMode"
-                    @delete-action="deleteAction" />
-            </template>
-            <template v-else-if="isAddingRating">
-                <RatingForm :rating="userRating[0]" :isEdit="!!userRating[0]" :beerId="beer._id"
-                    @submit="handleRatingSubmit" @cancel="toggleRatingForm" />
-            </template>
-            <template v-else>
-                <h2>{{ beer.name }}</h2>
-                <div class="button-group">
-                    <a :href="`https://www.systembolaget.se/sortiment/?q=${encodeURIComponent(beer.name)}`"
-                        class="button button-systembolaget" target="_blank" rel="noopener noreferrer">
-                        Systembolaget
-                    </a>
-                    <a :href="`https://untappd.com/search?q=${encodeURIComponent(beer.name)}`"
-                        class="button button-untappd" target="_blank" rel="noopener noreferrer">
-                        Untappd
-                    </a>
-                    <a :href="`https://www.ratebeer.com/search?beername=${encodeURIComponent(beer.name)}&tab=beer`"
-                        class="button button-ratebeer" target="_blank" rel="noopener noreferrer">
-                        Ratebeer
-                    </a>
-                </div>
-                <p><strong>Type:</strong> {{ beer.type }}</p>
-                <p v-if="beer.brewery"><strong>Brewery:</strong> {{ beer.brewery }}</p>
-                <p v-if="beer.abv"><strong>ABV:</strong> {{ beer.abv }}%</p>
-                <p v-if="beer.averageRating"><strong>Average Rating:</strong> {{ beer.averageRating }}</p>
-
-                <h3>Ratings</h3>
-                <ul v-if="ratings.length">
-                    <li v-for="(rating, index) in displayedRatings" :key="rating._id">
-                        <strong>{{ rating.user.username }}:</strong> {{ rating.score }} - {{ rating.comment }}
-                    </li>
-                </ul>
-                <p v-else>No ratings</p>
-
-                <!-- Toggle button to show/hide more ratings -->
-                <button v-if="ratings.length > 5" @click="toggleShowAllRatings" class="toggle-button">
-                    {{ showAllRatings ? 'Hide ratings' : 'Show All Ratings' }}
-                </button>
-                <button v-if="isLoggedIn && !isAddingRating" @click="toggleRatingForm" class="add-rating-button">Add
-                    Rating</button>
-            </template>
-
-            <button @click="closeModal" class="close-button">Close</button>
+  <div class="modal-overlay" @click.self="closeModal">
+    <div class="modal-content">
+      <button
+        v-if="!isEditing && !isAddingRating && isLoggedIn"
+        @click="toggleEditMode"
+        class="edit-button"
+      >
+        Edit
+      </button>
+      <template v-if="isEditing">
+        <!-- Use BeerForm for editing -->
+        <BeerForm
+          :beer="beer"
+          :isEdit="true"
+          @submit="handleFormSubmit"
+          @cancel="toggleEditMode"
+          @delete-action="deleteAction"
+        />
+      </template>
+      <template v-else-if="isAddingRating">
+        <RatingForm
+          :rating="userRating?.[0]"
+          :isEdit="!!userRating?.[0]"
+          :beerId="beer._id"
+          @submit="handleRatingSubmit"
+          @cancel="toggleRatingForm"
+        />
+      </template>
+      <template v-else>
+        <h2>{{ beer.name }}</h2>
+        <div class="button-group">
+          <a
+            :href="`https://www.systembolaget.se/sortiment/?q=${encodeURIComponent(beer.name)}`"
+            class="button button-systembolaget"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Systembolaget
+          </a>
+          <a
+            :href="`https://untappd.com/search?q=${encodeURIComponent(beer.name)}`"
+            class="button button-untappd"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Untappd
+          </a>
+          <a
+            :href="`https://www.ratebeer.com/search?beername=${encodeURIComponent(beer.name)}&tab=beer`"
+            class="button button-ratebeer"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ratebeer
+          </a>
         </div>
+        <p><strong>Type:</strong> {{ beer.type }}</p>
+        <p v-if="beer.brewery"><strong>Brewery:</strong> {{ beer.brewery }}</p>
+        <p v-if="beer.abv"><strong>ABV:</strong> {{ beer.abv }}%</p>
+        <p v-if="beer.averageRating"><strong>Average Rating:</strong> {{ beer.averageRating }}</p>
+
+        <h3>Ratings</h3>
+        <ul v-if="ratings.length">
+          <li v-for="(rating, index) in displayedRatings" :key="rating._id">
+            <strong>{{ rating.user.username }}:</strong> {{ rating.score }} - {{ rating.comment }}
+          </li>
+        </ul>
+        <p v-else>No ratings</p>
+
+        <!-- Toggle button to show/hide more ratings -->
+        <button v-if="ratings.length > 5" @click="toggleShowAllRatings" class="toggle-button">
+          {{ showAllRatings ? 'Hide ratings' : 'Show All Ratings' }}
+        </button>
+        <button
+          v-if="isLoggedIn && !isAddingRating"
+          @click="toggleRatingForm"
+          class="add-rating-button"
+        >
+          Add Rating
+        </button>
+      </template>
+
+      <button @click="closeModal" class="close-button">Close</button>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -59,229 +91,115 @@ import { defineComponent, type PropType, ref, computed, watch } from 'vue'
 import type { Beer, Rating } from '@/models/Beer'
 import BeerForm from '@/components/BeerForm.vue'
 import RatingForm from '@/components/RatingForm.vue'
-import { Myconsts } from '@/const';
+import { Myconsts } from '@/const'
 
 export default defineComponent({
-    name: 'BeerModal',
-    components: { BeerForm, RatingForm },
-    props: {
-        beer: {
-            type: Object as PropType<Beer>,
-            required: true
-        },
-        ratings: {
-            type: Array as PropType<Rating[]>,
-            required: true
-        }
+  name: 'BeerModal',
+  components: { BeerForm, RatingForm },
+  props: {
+    beer: {
+      type: Object as PropType<Beer>,
+      required: true
     },
-    emits: ['close-modal', 'update-beer'],
-    setup(props, { emit }) {
-        const showAllRatings = ref(false)
-        const isEditing = ref(false)
-        const isAddingRating = ref(false)
-        const isLoggedIn = ref(false);
-        const userRating = ref<Rating | null>(null)
-        const token = localStorage.getItem(Myconsts.tokenName);
-
-        isLoggedIn.value = !!token;
-
-        const displayedRatings = computed(() => {
-            return showAllRatings.value ? props.ratings : props.ratings.slice(0, 5)
-        })
-
-        const fetchUserRating = async () => {
-            if (!token || !props.beer || !props.beer._id) return;
-            try {
-                const response = await fetch(`/api/ratings/user-ratings/${props.beer._id}`, {
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                });
-                if (!response.ok) {
-                    throw new Error(`Error fetching user rating: ${response.statusText}`);
-                }
-                const data = await response.json();
-
-                userRating.value = data; // Assuming the response is the rating object
-            } catch (err) {
-                console.error('Failed to fetch user rating:', err);
-            }
-        }
-
-        // Watch for changes in the beer prop and fetch user ratings when it updates
-        watch(() => props.beer, (newBeer) => {
-            if (newBeer && newBeer._id) {
-                fetchUserRating();
-            }
-        }, { immediate: true });
-
-        const toggleShowAllRatings = () => {
-            showAllRatings.value = !showAllRatings.value
-        }
-
-        const toggleEditMode = () => {
-            isEditing.value = !isEditing.value
-        }
-
-        const toggleRatingForm = () => {
-            isAddingRating.value = !isAddingRating.value;
-        }
-
-        const handleFormSubmit = (updatedBeer: Partial<Beer>) => {
-            Object.assign(props.beer, updatedBeer)
-            emit('update-beer', updatedBeer)
-            toggleEditMode()
-        }
-
-        const handleRatingSubmit = (newRating: Rating) => {
-            /*if (newRating) {
-                userRating.value = newRating;
-            }
-            toggleRatingForm();*/
-            closeModal();
-        }
-
-        const closeModal = () => {
-            emit('close-modal')
-        }
-
-        const deleteAction = () => {
-            closeModal()
-        }
-
-        return {
-            showAllRatings,
-            isEditing,
-            displayedRatings,
-            toggleShowAllRatings,
-            toggleEditMode,
-            handleFormSubmit,
-            closeModal,
-            isLoggedIn,
-            deleteAction,
-            isAddingRating,
-            toggleRatingForm,
-            handleRatingSubmit,
-            userRating
-        }
+    ratings: {
+      type: Array as PropType<Rating[]>,
+      required: true
     }
+  },
+  emits: ['close-modal', 'update-beer'],
+  setup(props, { emit }) {
+    const showAllRatings = ref(false)
+    const isEditing = ref(false)
+    const isAddingRating = ref(false)
+    const isLoggedIn = ref(false)
+    const userRating = ref<Rating[] | null>(null) // Define as array of Rating or null
+    const token = localStorage.getItem(Myconsts.tokenName)
+
+    isLoggedIn.value = !!token
+
+    const displayedRatings = computed(() => {
+      return showAllRatings.value ? props.ratings : props.ratings.slice(0, 5)
+    })
+
+    const fetchUserRating = async () => {
+      if (!token || !props.beer || !props.beer._id) return
+      try {
+        const response = await fetch(`/api/ratings/user-ratings/${props.beer._id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        if (!response.ok) {
+          throw new Error(`Error fetching user rating: ${response.statusText}`)
+        }
+        const data = await response.json()
+
+        userRating.value = data // Assuming the response is an array of rating objects
+      } catch (err) {
+        console.error('Failed to fetch user rating:', err)
+      }
+    }
+
+    // Watch for changes in the beer prop and fetch user ratings when it updates
+    watch(
+      () => props.beer,
+      (newBeer) => {
+        if (newBeer && newBeer._id) {
+          fetchUserRating()
+        }
+      },
+      { immediate: true }
+    )
+
+    const toggleShowAllRatings = () => {
+      showAllRatings.value = !showAllRatings.value
+    }
+
+    const toggleEditMode = () => {
+      isEditing.value = !isEditing.value
+    }
+
+    const toggleRatingForm = () => {
+      isAddingRating.value = !isAddingRating.value
+    }
+
+    const handleFormSubmit = (updatedBeer: Partial<Beer>) => {
+      Object.assign(props.beer, updatedBeer)
+      emit('update-beer', updatedBeer)
+      toggleEditMode()
+    }
+
+    const handleRatingSubmit = (newRating: Rating) => {
+      /*if (newRating) {
+                  userRating.value = newRating;
+              }
+              toggleRatingForm();*/
+      closeModal()
+    }
+
+    const closeModal = () => {
+      emit('close-modal')
+    }
+
+    const deleteAction = () => {
+      closeModal()
+    }
+
+    return {
+      showAllRatings,
+      isEditing,
+      displayedRatings,
+      toggleShowAllRatings,
+      toggleEditMode,
+      handleFormSubmit,
+      closeModal,
+      isLoggedIn,
+      deleteAction,
+      isAddingRating,
+      toggleRatingForm,
+      handleRatingSubmit,
+      userRating
+    }
+  }
 })
 </script>
-<style scoped>
-.button-group {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 10px;
-    justify-content: center;
-    align-items: center;
-    margin-top: 10px;
-}
-
-.button {
-    display: inline-block;
-    padding: 8px 12px;
-    text-decoration: none;
-    color: white;
-    border-radius: 4px;
-    transition: background-color 0.3s ease;
-    font-size: 0.9em;
-}
-
-.button-systembolaget {
-    background-color: #004b9b;
-}
-
-.button-untappd {
-    background-color: #f8a800;
-}
-
-.button-ratebeer {
-    background-color: #4e4e4e;
-}
-
-.button:hover {
-    opacity: 0.9;
-}
-
-.modal-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.7);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 1000;
-}
-
-.modal-content {
-    background-color: white;
-    padding: 20px;
-    border-radius: 8px;
-    width: 90%;
-    max-width: 500px;
-    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-    position: relative;
-}
-
-h2 {
-    margin-top: 0;
-}
-
-.edit-button {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    padding: 8px 12px;
-    background-color: #2ecc71;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-}
-
-.edit-button:hover {
-    background-color: #27ae60;
-}
-
-.close-button,
-.toggle-button {
-    margin: .5rem;
-    padding: 10px 20px;
-    background-color: #3498db;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-}
-
-.close-button:hover,
-.toggle-button:hover {
-    background-color: #2980b9;
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    margin-bottom: 10px;
-}
-
-.add-rating-button {
-    margin-top: 20px;
-    padding: 10px 20px;
-    background-color: #2ecc71;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-}
-
-.add-rating-button:hover {
-    background-color: #27ae60;
-}
-</style>
--- beer-rating-client-vue/src/components/RatingForm.vue
+++ beer-rating-client-vue/src/components/RatingForm.vue
@@ -1,191 +1,210 @@
 <template>
-    <form @submit.prevent="handleSubmit" class="rating-form">
-        <div class="form-group">
-            <label for="score" class="form-label">Score: {{ form.score }}</label>
-            <!-- Slider for rating score -->
-            <input type="range" id="score" v-model="form.score" min="0" max="5" step="0.25" required class="slider" />
-        </div>
-        <div class="form-group">
-            <label for="comment" class="form-label">Comment</label>
-            <textarea id="comment" v-model="form.comment" required class="textarea"></textarea>
-        </div>
-        <div class="button-group">
-            <button type="submit" class="submit-button">{{ isEdit ? 'Update Rating' : 'Add Rating' }}</button>
-            <button type="button" @click="cancel" class="cancel-button">Cancel</button>
-        </div>
-    </form>
-    <div v-if="error" class="error-container">
-        <ErrorComponent :errorMessage="error" @retry="handleSubmit" />
+  <form @submit.prevent="handleSubmit" class="rating-form">
+    <div class="form-group">
+      <label for="score" class="form-label">Score: {{ form.score }}</label>
+      <!-- Slider for rating score -->
+      <input
+        type="range"
+        id="score"
+        v-model="form.score"
+        min="0"
+        max="5"
+        step="0.25"
+        required
+        class="slider"
+      />
     </div>
+    <div class="form-group">
+      <label for="comment" class="form-label">Comment</label>
+      <textarea id="comment" v-model="form.comment" required class="textarea"></textarea>
+    </div>
+    <div class="button-group">
+      <button type="submit" class="submit-button">
+        {{ isEdit ? 'Update Rating' : 'Add Rating' }}
+      </button>
+      <button type="button" @click="cancel" class="cancel-button">Cancel</button>
+    </div>
+  </form>
+  <div v-if="error" class="error-container">
+    <ErrorComponent :errorMessage="error" @retry="handleSubmit" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, type PropType, watch, toRefs } from 'vue'
 import ErrorComponent from '@/components/ErrorComponent.vue'
-import { Myconsts } from '@/const';
+import { Myconsts } from '@/const'
 
 export default defineComponent({
-    name: 'RatingForm',
-    props: {
-        rating: {
-            type: Object as PropType<{ score: number; comment: string; _id?: string }>,
-            default: () => ({ score: 0, comment: '' }),
-        },
-        isEdit: {
-            type: Boolean,
-            default: false,
-        },
-        beerId: {
-            type: String,
-            required: true,
-        },
+  name: 'RatingForm',
+  props: {
+    rating: {
+      type: Object as PropType<{ score: number; comment: string; _id?: string }>,
+      default: () => ({ score: 0, comment: '' })
     },
-    components: {
-        ErrorComponent
+    isEdit: {
+      type: Boolean,
+      default: false
     },
-    emits: ['submit', 'cancel'],
-    setup(props, { emit }) {
-        const error = ref<string | null>(null)
-        const { rating, isEdit } = toRefs(props)
-        const form = ref({ ...rating.value })
-
-        watch(isEdit, (newVal) => {
-            if (newVal) {
-                form.value = { ...rating.value }
-            } else {
-                form.value = { score: 0, comment: '' }
-            }
-        }, { immediate: true }) // Use immediate to ensure initial form value
-
-        const handleSubmit = async () => {
-            error.value = null
-            try {
-                const token = localStorage.getItem(Myconsts.tokenName)
-                if (!token) {
-                    throw new Error('User is not authenticated')
-                }
-
-                let response
-                if (isEdit.value && form.value._id) {
-                    // Update existing rating
-                    response = await fetch(`/api/ratings/${form.value._id}`, {
-                        method: 'PUT',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${token}`,
-                        },
-                        body: JSON.stringify({ score: form.value.score, comment: form.value.comment }),
-                    })
-                } else {
-                    // Add new rating
-                    response = await fetch(`/api/ratings`, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${token}`,
-                        },
-                        body: JSON.stringify({ beerId: props.beerId, score: form.value.score, comment: form.value.comment }),
-                    })
-                }
-
-                if (!response.ok) {
-                    const errorData = await response.json()
-                    throw new Error(errorData.message || 'Failed to submit rating')
-                }
-
-                const data = await response.json()
-                emit('submit', data)
-            } catch (err) {
-                console.error('Failed to submit rating:', err)
-                error.value = err instanceof Error ? err.message : 'An unknown error occurred.'
-            }
-        }
-
-        const cancel = () => {
-            emit('cancel')
-        }
-
-        return {
-            form,
-            handleSubmit,
-            cancel,
-            error
-        }
+    beerId: {
+      type: Number,
+      required: true
     }
+  },
+  components: {
+    ErrorComponent
+  },
+  emits: ['submit', 'cancel'],
+  setup(props, { emit }) {
+    const error = ref<string | null>(null)
+    const { rating, isEdit } = toRefs(props)
+    const form = ref({ ...rating.value })
+
+    watch(
+      isEdit,
+      (newVal) => {
+        if (newVal) {
+          form.value = { ...rating.value }
+        } else {
+          form.value = { score: 0, comment: '' }
+        }
+      },
+      { immediate: true }
+    ) // Use immediate to ensure initial form value
+
+    const handleSubmit = async () => {
+      error.value = null
+      try {
+        const token = localStorage.getItem(Myconsts.tokenName)
+        if (!token) {
+          throw new Error('User is not authenticated')
+        }
+
+        let response
+        if (isEdit.value && form.value._id) {
+          // Update existing rating
+          response = await fetch(`/api/ratings/${form.value._id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({ score: form.value.score, comment: form.value.comment })
+          })
+        } else {
+          // Add new rating
+          response = await fetch(`/api/ratings`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({
+              beerId: props.beerId,
+              score: form.value.score,
+              comment: form.value.comment
+            })
+          })
+        }
+
+        if (!response.ok) {
+          const errorData = await response.json()
+          throw new Error(errorData.message || 'Failed to submit rating')
+        }
+
+        const data = await response.json()
+        emit('submit', data)
+      } catch (err) {
+        console.error('Failed to submit rating:', err)
+        error.value = err instanceof Error ? err.message : 'An unknown error occurred.'
+      }
+    }
+
+    const cancel = () => {
+      emit('cancel')
+    }
+
+    return {
+      form,
+      handleSubmit,
+      cancel,
+      error
+    }
+  }
 })
 </script>
 <style scoped>
 .rating-form {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    padding: 20px;
-    border: 1px solid #ddd;
-    border-radius: 8px;
-    background-color: #f9f9f9;
-    max-width: 400px;
-    margin: 0 auto;
-    box-sizing: border-box;
-    /* Ensures padding and borders are included in the width */
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  background-color: #f9f9f9;
+  max-width: 400px;
+  margin: 0 auto;
+  box-sizing: border-box;
+  /* Ensures padding and borders are included in the width */
 }
 
 .form-group {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 }
 
 .form-label {
-    font-weight: bold;
-    margin-bottom: 5px;
-    color: #333;
+  font-weight: bold;
+  margin-bottom: 5px;
+  color: #333;
 }
 
 .slider {
-    width: 100%;
+  width: 100%;
 }
 
 .textarea {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 4px;
-    resize: vertical;
-    box-sizing: border-box;
-    /* Ensures padding and borders are included in the width */
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  resize: vertical;
+  box-sizing: border-box;
+  /* Ensures padding and borders are included in the width */
 }
 
 .button-group {
-    display: flex;
-    gap: 10px;
-    justify-content: space-between;
+  display: flex;
+  gap: 10px;
+  justify-content: space-between;
 }
 
 .submit-button,
 .cancel-button {
-    flex: 1;
-    padding: 10px 15px;
-    background-color: #3498db;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
+  flex: 1;
+  padding: 10px 15px;
+  background-color: #3498db;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
 }
 
 .submit-button:hover {
-    background-color: #2980b9;
+  background-color: #2980b9;
 }
 
 .cancel-button {
-    background-color: #e74c3c;
+  background-color: #e74c3c;
 }
 
 .cancel-button:hover {
-    background-color: #c0392b;
+  background-color: #c0392b;
 }
 
 .error-container {
-    margin-top: 15px;
+  margin-top: 15px;
 }
 </style>
